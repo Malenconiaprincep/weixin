e5a9a9f7133ecf6b32f9805e4c9c8ad7618323b4
makuta <279495889@qq.com>
2015-01-27 14:09:01 +0800

    update

--- modules/index.js
+++ modules/index.js
@@ -62,7 +62,7 @@ var get_jsapi_ticket = function() {
 
 var fetch = function() {
   return function(done) {
-    var config = sign(ticket, 'http://piaoshihuang.cn');
+    var config = sign(ticket, 'piaoshihuang.cn');
     this.result = {
       ret_code: 0,
       config: _.extend(config, {
