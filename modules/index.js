af46dea3b23b9e6a0a7175c4297e68284fc5add2
makuta <279495889@qq.com>
2015-02-12 00:27:06 +0800

    update

--- modules/index.js
+++ modules/index.js
@@ -27,35 +27,41 @@ var appSecret = 'fb46e607c25aff850c6c5d65efbe4a00'
    */
 var token;
 var ticket;
-
+var resp;
 
 var access_token = function() {
   return function(done) {
-    var token_url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + appId + '&secret=' + appSecret
-    var self = this;
-    console.log(token_url);
+    var token_url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + appId + '&secret=' + appSecret;
     request(token_url, function(error, response, body) {
       if (!error && response.statusCode == 200) {
-        logger.info('access_token');
-        logger.info(body);
-        token = JSON.parse(body).access_token;
+        resp = JSON.parse(body);
       }
-      done();
+      done(null, resp);
     })
   }
 }
 
 
-var get_jsapi_ticket = function() {
+var get_jsapi_ticket = function(resp) {
   return function(done) {
-    var jsapi_ticket_url = 'https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=' + token + '&type=jsapi';
+    var self = this;
+    var jsapi_ticket_url = 'https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=' + resp.access_token + '&type=jsapi';
     request(jsapi_ticket_url, function(error, response, body) {
       if (!error && response.statusCode == 200) {
-        logger.info('get_jsapi_ticket');
-        logger.info(body);
-        ticket = JSON.parse(body).ticket;
-        done();
+        resp = JSON.parse(body);
       }
+      var ticket = resp.ticket;
+      var url = 'http://piaoshihuang.cn/';
+      var config = sign(ticket, url);
+      logger.info(config);
+      self.result = {
+        nonceStr: config.nonceStr,
+        timestamp: config.timestamp,
+        appid: appId,
+        signature: config.signature,
+        url: url
+      };
+      done();
     })
   }
 }
@@ -65,7 +71,7 @@ var get_jsapi_ticket = function() {
 var fetch = function() {
   return function(done) {
     if (!cache.config) {
-      cache.config = sign(ticket, 'http://piaoshihuang.cn');
+      cache.config = sign(ticket, 'http://piaoshihuang.cn/');
     }
     this.set('Access-Control-Allow-Origin', '*');
     this.set('Access-Control-Allow-Credentials', 'true');
@@ -87,10 +93,11 @@ module.exports = {
         yield resetctx.call(this);
         this.json = true;
         if (!ticket) {
-          yield access_token.call(this);
-          // yield get_jsapi_ticket.call(this);
+          var resp =
+            yield access_token.call(this);
+          yield get_jsapi_ticket.call(this, resp);
         }
-        yield fetch.call(this);
+        // yield fetch.call(this);
         yield response.call(this);
       }),
       app.route('/demo$').get(function*(next) {
